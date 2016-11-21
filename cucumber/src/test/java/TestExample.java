19fa77c3128673da70fcccec220138047fbff349
Sergiu Postolachi <sergiu.postolachi@import.io>
2016-11-21 10:25:02 +0200

    read file from url

--- cucumber/src/test/java/TestExample.java
+++ cucumber/src/test/java/TestExample.java
@@ -1,4 +1,10 @@
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,10 +12,24 @@ import static org.junit.Assert.assertEquals;
  * @author sergiupostolachi
  */
 public class TestExample {
+	public static final String configFileDir = System.getProperty("configDir");
 	@Test
-	public void testSome(){
-		System.out.println("Test");
-		assertEquals(1,1);
+	public void testSome() throws IOException {
+		System.out.println(configFileDir);
+		String read = readContent();
+		assertEquals(read,"user.name=qatestuser");
+	}
+
+
+	private String readContent() throws IOException {
+
+		InputStream input = new URL(configFileDir).openStream();
+		try {
+			return IOUtils.toString( input );
+		} finally {
+			IOUtils.closeQuietly(input);
+		}
+
 	}
 
 	@Test

509d6a5f01abb82655efb83847fa53b3da4eff5e
Sergiu Postolachi <sergiu.postolachi@import.io>
2016-11-21 09:08:22 +0200

    Test

---
+++ cucumber/src/test/java/TestExample.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author sergiupostolachi
+ */
+public class TestExample {
+	@Test
+	public void testSome(){
+		System.out.println("Test");
+		assertEquals(1,1);
+	}
+
+}
